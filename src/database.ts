a62cb6adac530176cfb1c0444332d3a4a32f7ece
noviceGuru <amin.arabi@gmail.com>
2022-12-20 17:43:14 +0100

    module export fix

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,6 @@
 import { Sequelize } from "sequelize"
 
-const sequelize = new Sequelize('test-db', 'user', 'password', {
+export const sequelize = new Sequelize('test-db', 'user', 'password', {
     dialect: 'sqlite',
     host: './dev.sqlite'
 })
-
-export { sequelize }
